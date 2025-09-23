023dd924205af14905f7be6b95ad4050e0784b15
mac_chang <1162618418@qq.com>
2025-09-23 22:50:00 +0800

    更新markdown

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# s1a
-SAR
+# this is Nat sat README file
+Nansat is a Python library for working with satellite data in a simple and efficient way.
+We have added some common functionalities based on nansat.
+# TODO 
+- [x] convert db
+- [ ] filter
+  -  [ ] Lee
+  -  [ ] Frost
+  -  [ ] Kuan
+  -  [ ] Gamma MAP
+- [ ] GF3 preprocess
+- [ ] Geometric Correction
+
+
+
+
--- read_write.py
+++ read_write.py
@@ -10,36 +10,7 @@ infileName = "/Users/chang/code/S1A/data/S1A_EW_GRDM_1SDH_20221201T153651_202212
 outfileName_method1 = "/Users/chang/code/S1A/data/S1A_EW_GRDM_1SDH_20221201T153651_20221201T153746_046140_058606_F0E71.tif"
 outfileName_method2 = "/Users/chang/code/S1A/data/S1A_EW_GRDM_1SDH_20221201T153651_20221201T153746_046140_058606_F0E72.tif"
 outfileName_2bands = "/Users/chang/code/S1A/data/S1A_EW_GRDM_1SDH_20221201T153651_20221201T153746_046140_058606_F0E_bands2.tif"
-
-def hh_angular_correction(n, img, bandName, correct_hh_factor):
-    """ Correct sigma0_HH for incidence angle dependence
-
-    Paramaters
-    ----------
-    correct_hh_factor : float
-        coefficient in the correction factor sigma0_HH_cor = sigma0_HH + correct_hh_factor * incidence_angle
-
-    Returns
-    -------
-    img : ndarray
-        corrected sigma0_HH in dB
-
-    """
-    if bandName == 'sigma0_HH' and n.has_band('incidence_angle'):
-        ia = n['incidence_angle']
-        imgcor = img - ia * correct_hh_factor
-    else:
-        imgcor = img
-
-    return imgcor
-
-# n = Nansat(infileName, mapper='sentinel1_l1',fast = False)
-# data = n.get_GDALRasterBand(bandNo)
-# # print(np.unique(data.ReadAsArray()))
-# # n.list_bands()
-# n.write_geotiffimage(outfileName_method1, bandNo)
-# n.write_geotiff(outfileName_method2,bandNo)
-# n.write_geotiff_mutilband(outfileName_method3,bandNum)
+# 初始化数据
 n = Nansat(infileName)
 # 提取波段数据1，转换为db数据
 imghh = n[bandNoHH]
@@ -70,5 +41,9 @@ print(nout.bands())
 nout.write_geotiffband(outfileName_method1)
 
 # nout.write_geotiffimage(outfileName_method3,'sigma_hv')
+'''
+读取zip文件后将数据，通过nasat类的方法实现波段提取、db数据转换、
+去噪、保存为tif文件、波段数据提取、保存处理后的结果。
+'''
 
 
